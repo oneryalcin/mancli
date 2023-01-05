24c0749c5a957b99e4577ce2c1781c9a5d9eb59f
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2023-01-05 16:23:19 +0000

    Add second behave test

--- features/cli_hello_command.feature
+++ features/cli_hello_command.feature
@@ -3,3 +3,7 @@ Feature: Testing `hello` argument for cli
   Scenario: Run only with hello
       When user runs command klickbrick hello
       Then CLI prints `hello world`
+
+  Scenario: Run with hello and name argument Oner
+      When user runs command klickbrick hello --name Oner
+      Then CLI prints `hello Oner`
--- features/steps/cli_hello_command.py
+++ features/steps/cli_hello_command.py
@@ -19,3 +19,21 @@ def step_impl(context):
     :type context: behave.runner.Context
     """
     assert context.result.stdout == "hello world\n"
+
+
+@when("user runs command klickbrick hello --name Oner")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    context.result = subprocess.run(
+        ["poetry", "run", "klickbrick", "hello", "--name", "Oner"], capture_output=True, text=True
+    )
+
+
+@then("CLI prints `hello Oner`")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    assert context.result.stdout == "hello Oner\n"
