b36822e9de5aa55c3a76f6cfe40417d598a28ffe
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2023-01-05 14:14:14 +0000

    Remove unneded code

--- mancli/klick_brick_cli.py
+++ mancli/klick_brick_cli.py
@@ -1,36 +1,6 @@
 import argparse
 import sys
 
-#
-# def run():
-#     if not hasattr(args, "name"):
-#         print("Hello World")
-#     elif not args.name:
-#         print("Hello World")
-#     else:
-#         print(f"Hello {args.name}")
-#
-#
-# def parse_args(args):
-#     global_parser = argparse.ArgumentParser(
-#         prog="klickbrick",
-#         description="Some ClI app",
-#         epilog="Thanks for using %(prog)s!"
-#     )
-#
-#     subparsers = global_parser.add_subparsers(
-#         title="subcommands", help="arithmetic operations"
-#     )
-#
-#     name_parser = subparsers.add_parser("hello", help="say hello to somebody")
-#     name_parser.add_argument("-n", "--name", help="User name")
-#     name_parser.set_defaults(func=run)
-#
-#     return global_parser.parse_args(args)
-#
-#
-# args = parse_args(sys.argv[1:])
-
 
 def parse_args(args):
     parser = argparse.ArgumentParser()
